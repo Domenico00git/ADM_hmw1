f473088582b69204d11fa4d3b703a3f3a3d07f33
Domenico00git <139566600+Domenico00git@users.noreply.github.com>
2023-11-07 00:51:49 +0100

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -1,1 +1,21 @@
+# Say "Hello, World!" With Python
+if __name__ == '__main__':
+    print("Hello, World!")
 
+# Python If-Else
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if (n%2 != 0):
+        print("Weird")
+    elif (n>=2 and n<=5):
+        print("Not Weird")
+    elif (n>=6 and n<=20):
+        print("Weird")
+    elif (n>=20):
+        print("Not Weird")
